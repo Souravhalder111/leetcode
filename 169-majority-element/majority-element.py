3a5cea3a616e5d879510da60aa62085599d985fc
Souravhalder111 <souravhalder.mid@gmail.com>
2026-08-18 21:05:27 +0530

    Time: 16 ms (10.06%) | Memory: 21.2 MB (47.07%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        hashmap = {}
+        for i in range(n):
+            if nums[i] in hashmap:
+                hashmap[nums[i]] += 1
+            else:
+                hashmap[nums[i]] = 1
+        for num in hashmap:
+            if(hashmap[num] > n/2):
+                return num
+        return -1
